1716c12ae7c986fd78331365ed00a2f6d438fc61
ERIC YI <eyi@andrew.cmu.edu>
2015-04-11 23:56:09 -0400

    add icon

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-	$('.events').each(function() {
-		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$(this).css('background-color', randomColorChange);
-	});
-	$('.my-event-picture').each(function() {
-		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$(this).css('background-color', randomColorChange);
-	});
-});
+// $(document).ready(function() {
+// 	$('.events').each(function() {
+// 		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+// 		$(this).css('background-color', randomColorChange);
+// 	});
+// 	$('.my-event-picture').each(function() {
+// 		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+// 		$(this).css('background-color', randomColorChange);
+// 	});
+// });
