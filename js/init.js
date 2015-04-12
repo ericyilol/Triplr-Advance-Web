fb75422b7098aee8f6de92a2aac098fbfefd40fd
Mingrui Zhang <mingruizhang50@gmail.com>
2015-04-12 12:38:13 -0400

    4-12

--- js/init.js
+++ js/init.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
-	$('.events').each(function() {
-		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$(this).css('background-color', randomColorChange);
-	});
-	$('.my-event-picture').each(function() {
-		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$(this).css('background-color', randomColorChange);
-	});
-});
+(function () {
+    var version = '1.0';
+    var scripts = [
+        'js/random-color.js',
+        'js/trips-module.js',
+        'js/lib/jquery.ui.min.js',
+        'js/lib/bootstrap.min.js'
+    ];
+
+    var temp = [];
+    for (var i = 0; i < scripts.length; i ++) {
+        temp.push('<script src="' + scripts[i] + '?' + version +'"><\/script>');
+    }
+    document.write(temp.join(''));
+})();
