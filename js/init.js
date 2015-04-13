0e06253b8b26c1293adc7b5cef134cb031393d86
ERIC YI <eyi@andrew.cmu.edu>
2015-04-13 18:41:55 -0400

    try home page js

--- js/init.js
+++ js/init.js
@@ -8,7 +8,8 @@ var WINDOW_WIDTH  = $(window).width();
         'js/trips-module.js',
         'js/lib/jquery.ui.min.js',
         'js/lib/bootstrap.min.js',
-        'js/dashboard.js'
+        'js/dashboard.js',
+        'js/home-page.js'
     ];
 
     var temp = [];
@@ -19,20 +20,20 @@ var WINDOW_WIDTH  = $(window).width();
 })();
 
 
-$(document).ready(function() { 
+// $(document).ready(function() { 
 
-	$('#event1').css("background-image", "url(./photo/egypt.jpg)"); 
-	$('#event2').css("background-image", "url(./photo/paris.jpg)"); 
-	$('#event3').css("background-image", "url(./photo/agra.jpg)"); 
-	$('#event4').css("background-image", "url(./photo/seoul.jpg)"); 
-	$('#event5').css("background-image", "url(./photo/prague.jpg)"); 
-	$('#event6').css("background-image", "url(./photo/taipei.jpg)"); 
-	$('#event7').css("background-image", "url(./photo/sudan.jpg)"); 
-	$('#event8').css("background-image", "url(./photo/madrid.jpg)"); 
-	$('#icon1').css("background-image", "url(./icon/icon-bus.png)");
-	$('#icon2').css("background-image", "url(./icon/icon-dine.png)");
+// 	$('#event1').css("background-image", "url(./photo/egypt.jpg)"); 
+// 	$('#event2').css("background-image", "url(./photo/paris.jpg)"); 
+// 	$('#event3').css("background-image", "url(./photo/agra.jpg)"); 
+// 	$('#event4').css("background-image", "url(./photo/seoul.jpg)"); 
+// 	$('#event5').css("background-image", "url(./photo/prague.jpg)"); 
+// 	$('#event6').css("background-image", "url(./photo/taipei.jpg)"); 
+// 	$('#event7').css("background-image", "url(./photo/sudan.jpg)"); 
+// 	$('#event8').css("background-image", "url(./photo/madrid.jpg)"); 
+// 	$('#icon1').css("background-image", "url(./icon/icon-bus.png)");
+// 	$('#icon2').css("background-image", "url(./icon/icon-dine.png)");
 
-});
+// });
 
 
 function navigation_init() {
