89d7c70dc9862e89b63c97639af0012f68c5c1ee
Mingrui Zhang <mingruizhang50@gmail.com>
2015-04-13 00:22:22 -0400

    dashboard

--- js/init.js
+++ js/init.js
@@ -15,22 +15,8 @@
     document.write(temp.join(''));
 })();
 
-$(document).ready(function() {
-	$('.event').each(function() {
-		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$(this).css('background-color', randomColorChange);
-	});
-	$('.my-event-picture').each(function() {
-		var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		$(this).css('background-color', randomColorChange);
-	});
 
-
-	$('#event2').css("background-image", "url(./photo/photo-egypt.jpg)");  
-
-    var cw = $('.my-events').width();
-    $('.my-events').css({'height':cw+'px'});
-
+$(document).ready(function() { 
 
 	$('#event1').css("background-image", "url(./photo/egypt.jpg)"); 
 	$('#event2').css("background-image", "url(./photo/paris.jpg)"); 
@@ -40,9 +26,39 @@ $(document).ready(function() {
 	$('#event6').css("background-image", "url(./photo/taipei.jpg)"); 
 	$('#event7').css("background-image", "url(./photo/sudan.jpg)"); 
 	$('#event8').css("background-image", "url(./photo/madrid.jpg)"); 
-
-
 	$('#icon1').css("background-image", "url(./icon/icon-bus.png)");
 	$('#icon2').css("background-image", "url(./icon/icon-dine.png)");
 
 });
+
+function dashboard_init() {
+    var eventHeight = $('.my-events').width();
+    $('.my-events').css({'height':eventHeight+'px'});
+
+    var timeLineHeight = $('.trips-planning').parent().height() - 100;
+    var timeLinePosition = $('.trips-planning').parent().position();
+    var timeLineTop = timeLinePosition.top + 40;
+    var timeLineLeft = timeLinePosition.left + 40;
+
+
+    $('.trips-planning .trips-planning-timeline').css({'height':timeLineHeight+'px'});
+    $('.trips-planning').css({'left':timeLineLeft+'px'});
+    $('.trips-planning').css({'top':timeLineTop+'px'});
+
+    $( '.trips-gone' ).each(function() {
+        var id_name = $(this).find("p").text();
+        id_name = "year-" + id_name;
+        console.log(id_name);
+        $(this).attr('id', id_name);
+        timeLineHeight = $('#' + id_name).parent().height() - 50;
+        timeLinePosition = $('#' + id_name).parent().position();
+        timeLineTop = timeLinePosition.top + 40;
+        timeLineLeft = timeLinePosition.left + 40;
+        $('#' + id_name).find('.trips-gone-timeline').css({'height':timeLineHeight+'px'});
+        $('#' + id_name).css({'left':timeLineLeft+'px'});
+        $('#' + id_name).css({'top':timeLineTop+'px'});
+    });
+    var oldestYear = $('.trips-gone-timeline').last().height();
+    oldestYear = oldestYear - 50;
+    $('.trips-gone-timeline').last().css({'height':oldestYear+'px'});
+}
